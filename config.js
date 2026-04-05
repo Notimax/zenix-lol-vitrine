1beec54bf69826a1cd45f89b4df87c9fe36f0b3a
Notimax <148656362+Notimax@users.noreply.github.com>
2026-04-05 23:11:51 +0200

    update active url to https://zenix.best/

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-window.ZENIX_ACTIVE_URL = "https://zenix.best1111111";
-window.ZENIX_PREVIOUS_URL = "https://zenix.best1";
+window.ZENIX_ACTIVE_URL = "https://zenix.best/";
+window.ZENIX_PREVIOUS_URL = "https://zenix.best1111111";
 window.ZENIX_LAST_UPDATED = "2026-04-05";
